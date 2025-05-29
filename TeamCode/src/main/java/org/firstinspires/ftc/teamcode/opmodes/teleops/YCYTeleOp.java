7ed371ca4a2ad5efffee9c3bef488b7dfb7f3ae0
zihanpeter <luzihan1104@163.com>
2025-05-29 22:45:20 +0800

    Pedro TeleOp Drive Commandbase UNVERIFIED

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/YCYTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/YCYTeleOp.java
@@ -9,42 +9,29 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.drive.Drive;
-import org.firstinspires.ftc.teamcode.subsystems.drive.constants.FConstants;
-import org.firstinspires.ftc.teamcode.subsystems.drive.constants.LConstants;
+import org.firstinspires.ftc.teamcode.subsystems.drive.TeleopMovement;
 import org.firstinspires.ftc.teamcode.utils.FunctionalButton;
 
 @TeleOp(name = "YCYTeleOp")
 public class YCYTeleOp extends CommandOpMode {
-    private Drive drive;
+    public Follower follower;
     private final Pose startPose = new Pose(0, 0, 0);
     private GamepadEx gamepadEx1;
 
     @Override
     public void initialize() {
-        drive = new Drive(hardwareMap, startPose);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
         gamepadEx1 = new GamepadEx(gamepad1);
 
-        new FunctionalButton(() -> Math.abs(gamepadEx1.getLeftX()) != 0 ||
-                Math.abs(gamepadEx1.getLeftY()) != 0 ||
-                Math.abs(gamepadEx1.getRightX()) != 0)
-                .whenPressed(
-                        drive.teleopDriveCommand(
-                                gamepadEx1.getLeftX(),
-                                gamepadEx1.getLeftY(),
-                                gamepadEx1.getRightX()
-                        )
-                );
+        new TeleopMovement(follower, false, gamepadEx1);
 
         new FunctionalButton(() -> gamepadEx1.getButton(GamepadKeys.Button.RIGHT_STICK_BUTTON))
-                .whenPressed(
-                        drive.resetPoseCommand(startPose)
-                );
+                .whenPressed(new InstantCommand(() -> follower.setStartingPose(startPose)));
     }
 
     @Override
     public void run() {
-        drive.follower.startTeleopDrive();
         CommandScheduler.getInstance().run();
     }
 }
